f38c57b48b6c758310d72bf2ebbbb1abeb77454c
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2022-10-13 19:40:08 -0400

    Fixed bumping.js

--- src/util/modules/bumping.js
+++ src/util/modules/bumping.js
@@ -4,7 +4,7 @@ class Bumping {
     static async message(client, message) {
         if (!message.interaction) return;
 
-        if (message.author.id === client.user.id) {
+        if (message.author.id === "302050872383242240" && message.interaction.commandName === "bump") {
             const user = message.interaction.user.id;
 
             const embed = new EmbedBuilder()
@@ -13,7 +13,7 @@ class Bumping {
                 .setColor("#FFBF40")
 
             message.fetch().catch(() => { });
-            message.reply({ embeds: [embed] })
+            message.reply({ embeds: [embed] }).catch(() => { })
 
             let interval = setInterval(() => {
                 const bumps = new EmbedBuilder()
